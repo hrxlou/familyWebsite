e169d563cb4b5aacae5e54a1aa958bc531791e4c
hrxlou <kimhyun4103@gmail.com>
2026-03-13 15:56:34 +0900

    업로드 폴더 통합: 루트의 uploads 폴더를 static/uploads로 이동 및 설정 업데이트

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', 'uploads')
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', os.path.join(basedir, 'static', 'uploads'))
 
     ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
     if not ADMIN_PASSWORD:
